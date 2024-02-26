f815aff6d04e652d83b2fa2f153473bab098ff4e
mfAline <159387767+mfAline@users.noreply.github.com>
2024-02-25 23:30:41 -0300

    Update desafios.js

--- desafios.js
+++ desafios.js
@@ -1,3 +1,4 @@
+//Desafio surpresa (iniciante)
 //Crie um código para armazenar o máximo de informações possível dos 
 //pokemon da imagem ao lado e ao final, para cada um, exiba uma 
 //mensagem de saída escrita o nome do pokemon concatenado com "Cadastrado com sucesso"
